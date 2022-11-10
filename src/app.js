8211ad1863cca39a0508774be68409ba2fed0897
Anvar <lapasovanvar2908@gmail.com>
2022-11-10 11:59:46 +0500

    project is updated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("code");
+const express = require("express")
+
+const PORT = process.env.PORT || 9898
+
+const app = express()
+
+app.listen(PORT, () => console.log("Server is running on port " + PORT))
